99760d944ad9d12c5ccdefb60e14941c2795e1b7
masamoto yamashiro <masamoto@protonmail.com>
2025-07-19 09:29:44 +0900

    Update mobile.js

--- js/gui/mobile.js
+++ js/gui/mobile.js
@@ -1,1 +1,37 @@
+export class MobileHandler {
+    constructor() {
+        this.elements = {};
+    }
 
+    init(elements) {
+        this.elements = elements;
+        this.updateView('input');
+    }
+
+    isMobile() {
+        return window.innerWidth <= 768;
+    }
+
+    updateView(mode) {
+        if (!this.isMobile()) {
+            // デスクトップモードでは全て表示
+            Object.values(this.elements).forEach(el => {
+                if (el) el.style.display = '';
+            });
+            return;
+        }
+        
+        // モバイルモード
+        if (mode === 'input') {
+            this.elements.inputArea.style.display = 'block';
+            this.elements.outputArea.style.display = 'none';
+            this.elements.memoryArea.style.display = 'none';
+            this.elements.dictionaryArea.style.display = 'block';
+        } else {
+            this.elements.inputArea.style.display = 'none';
+            this.elements.outputArea.style.display = 'block';
+            this.elements.memoryArea.style.display = 'block';
+            this.elements.dictionaryArea.style.display = 'none';
+        }
+    }
+}
