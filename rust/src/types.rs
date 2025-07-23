34e57941fd292baf54185d8b6756308293bcf9fd
masamoto yamashiro <masamoto@protonmail.com>
2025-07-24 07:18:22 +0900

    Update types.rs

--- rust/src/types.rs
+++ rust/src/types.rs
@@ -7,7 +7,6 @@ use rug::Rational;
 use crate::interpreter::Interpreter;
 
 // --- 意味記憶 (Semantic Memory) ---
-// 辞書に登録される実行可能なエンティティ
 #[derive(Clone)]
 pub enum Word {
     Builtin(WordFunc),
@@ -23,22 +22,22 @@ impl std::fmt::Debug for Word {
     }
 }
 
-// WordがPartialEqを実装するため、手動で比較ロジックを定義
+// Corrected manual implementation of PartialEq for Word
 impl PartialEq for Word {
     fn eq(&self, other: &Self) -> bool {
         match (self, other) {
-            (Word::Builtin(f1), Word::Builtin(f2)) => (f1 as *const ()) == (f2 as *const ()),
+            (Word::Builtin(f1), Word::Builtin(f2)) => std::ptr::eq(*f1 as *const (), *f2 as *const ()),
             (Word::UserDefined(v1), Word::UserDefined(v2)) => Rc::ptr_eq(v1, v2),
             _ => false,
         }
     }
 }
 
+
 pub type WordFunc = fn(&mut Interpreter) -> Result<(), String>;
 pub type Dictionary = HashMap<String, Rc<Word>>;
 
 // --- トークン ---
-// ソースコードを解析した結果の最小単位
 #[derive(Debug, Clone, PartialEq)]
 pub enum Token {
     Word(String),
@@ -51,22 +50,18 @@ pub enum Token {
 }
 
 // --- スタック上の型 ---
-// スタックに積まれる情報の種類
 #[derive(Debug, Clone, PartialEq)]
 pub enum Type {
     // --- データ型 (エピソード記憶) ---
-    // これらは純粋なデータとしてスタックに積まれる
     Number(Rc<Rational>),
     String(Rc<String>),
     Bool(bool),
-    Symbol(Rc<String>), // ワード名そのものをデータとして扱う
+    Symbol(Rc<String>),
     Vector(Rc<RefCell<Vec<Type>>>),
 
     // --- 手続き記憶 (実行可能な計画) ---
-    // IFやCALLなどの制御構造で使われる、即時実行可能なコードブロック
     Quotation(Rc<Vec<Token>>),
 
     // --- 意味記憶への参照 ---
-    // 辞書に登録されたワードそのもの
     Word(Rc<Word>),
 }
