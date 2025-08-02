70da9319e0000e80c5937c5633e129dd1d406a7b
masamoto yamashiro <masamoto@protonmail.com>
2025-08-02 18:18:18 +0900

    Update ajisai_core.d.ts

--- js/pkg/ajisai_core.d.ts
+++ js/pkg/ajisai_core.d.ts
@@ -1,57 +1,72 @@
 /* tslint:disable */
 /* eslint-disable */
+
+export interface ExecuteResult {
+    status: string;
+    output?: string;
+    autoNamed?: boolean;
+    autoNamedWord?: string;
+}
+
+export interface StepResult {
+    hasMore: boolean;
+    output?: string;
+    position?: number;
+    total?: number;
+}
+
 export class AjisaiInterpreter {
-  free(): void;
-  constructor();
-  execute(code: string): any;
-  init_step(code: string): string;
-  step(): any;
-  get_stack(): any;
-  get_register(): any;
-  get_custom_words(): string[];
-  get_custom_words_with_descriptions(): any;
-  get_custom_words_info(): any;
-  reset(): void;
-  save_table(name: string, schema: any, records: any): void;
-  load_table(name: string): any;
-  get_all_tables(): string[];
-  restore_stack(stack_js: any): void;
-  restore_register(register_js: any): void;
-  get_word_definition(name: string): any;
-  restore_word(name: string, definition: string, description?: string | null): void;
+    free(): void;
+    constructor();
+    execute(code: string): ExecuteResult;
+    init_step(code: string): string;
+    step(): StepResult;
+    get_stack(): any[];
+    get_register(): any;
+    get_custom_words(): string[];
+    get_custom_words_with_descriptions(): Array<[string, string | null]>;
+    get_custom_words_info(): Array<[string, string | null, boolean]>;
+    reset(): void;
+    save_table(name: string, schema: any, records: any): void;
+    load_table(name: string): any;
+    get_all_tables(): string[];
+    restore_stack(stack_js: any[]): void;
+    restore_register(register_js: any): void;
+    get_word_definition(name: string): string | null;
+    restore_word(name: string, definition: string, description?: string | null): void;
 }
 
 export type InitInput = RequestInfo | URL | Response | BufferSource | WebAssembly.Module;
 
 export interface InitOutput {
-  readonly memory: WebAssembly.Memory;
-  readonly __wbg_ajisaiinterpreter_free: (a: number, b: number) => void;
-  readonly ajisaiinterpreter_new: () => number;
-  readonly ajisaiinterpreter_execute: (a: number, b: number, c: number) => [number, number, number];
-  readonly ajisaiinterpreter_init_step: (a: number, b: number, c: number) => [number, number, number, number];
-  readonly ajisaiinterpreter_step: (a: number) => [number, number, number];
-  readonly ajisaiinterpreter_get_stack: (a: number) => any;
-  readonly ajisaiinterpreter_get_register: (a: number) => any;
-  readonly ajisaiinterpreter_get_custom_words: (a: number) => [number, number];
-  readonly ajisaiinterpreter_get_custom_words_with_descriptions: (a: number) => any;
-  readonly ajisaiinterpreter_get_custom_words_info: (a: number) => any;
-  readonly ajisaiinterpreter_reset: (a: number) => void;
-  readonly ajisaiinterpreter_save_table: (a: number, b: number, c: number, d: any, e: any) => [number, number];
-  readonly ajisaiinterpreter_load_table: (a: number, b: number, c: number) => any;
-  readonly ajisaiinterpreter_get_all_tables: (a: number) => [number, number];
-  readonly ajisaiinterpreter_restore_stack: (a: number, b: any) => [number, number];
-  readonly ajisaiinterpreter_restore_register: (a: number, b: any) => [number, number];
-  readonly ajisaiinterpreter_get_word_definition: (a: number, b: number, c: number) => any;
-  readonly ajisaiinterpreter_restore_word: (a: number, b: number, c: number, d: number, e: number, f: number, g: number) => [number, number];
-  readonly __wbindgen_exn_store: (a: number) => void;
-  readonly __externref_table_alloc: () => number;
-  readonly __wbindgen_export_2: WebAssembly.Table;
-  readonly __wbindgen_malloc: (a: number, b: number) => number;
-  readonly __wbindgen_realloc: (a: number, b: number, c: number, d: number) => number;
-  readonly __externref_table_dealloc: (a: number) => void;
-  readonly __wbindgen_free: (a: number, b: number, c: number) => void;
-  readonly __externref_drop_slice: (a: number, b: number) => void;
-  readonly __wbindgen_start: () => void;
+    readonly memory: WebAssembly.Memory;
+    readonly __wbg_ajisaiinterpreter_free: (a: number, b: number) => void;
+    readonly ajisaiinterpreter_new: () => number;
+    readonly ajisaiinterpreter_execute: (a: number, b: number, c: number) => [number, number, number];
+    readonly ajisaiinterpreter_init_step: (a: number, b: number, c: number) => [number, number, number, number];
+    readonly ajisaiinterpreter_step: (a: number) => [number, number, number];
+    readonly ajisaiinterpreter_get_stack: (a: number) => any;
+    readonly ajisaiinterpreter_get_register: (a: number) => any;
+    readonly ajisaiinterpreter_get_custom_words: (a: number) => [number, number];
+    readonly ajisaiinterpreter_get_custom_words_with_descriptions: (a: number) => any;
+    readonly ajisaiinterpreter_get_custom_words_info: (a: number) => any;
+    readonly ajisaiinterpreter_reset: (a: number) => void;
+    readonly ajisaiinterpreter_save_table: (a: number, b: number, c: number, d: any, e: any) => [number, number];
+    readonly ajisaiinterpreter_load_table: (a: number, b: number, c: number) => any;
+    readonly ajisaiinterpreter_get_all_tables: (a: number) => [number, number];
+    readonly ajisaiinterpreter_restore_stack: (a: number, b: any) => [number, number];
+    readonly ajisaiinterpreter_restore_register: (a: number, b: any) => [number, number];
+    readonly ajisaiinterpreter_get_word_definition: (a: number, b: number, c: number) => any;
+    readonly ajisaiinterpreter_restore_word: (a: number, b: number, c: number, d: number, e: number, f: number, g: number) => [number, number];
+    readonly __wbindgen_exn_store: (a: number) => void;
+    readonly __externref_table_alloc: () => number;
+    readonly __wbindgen_export_2: WebAssembly.Table;
+    readonly __wbindgen_malloc: (a: number, b: number) => number;
+    readonly __wbindgen_realloc: (a: number, b: number, c: number, d: number) => number;
+    readonly __externref_table_dealloc: (a: number) => void;
+    readonly __wbindgen_free: (a: number, b: number, c: number) => void;
+    readonly __externref_drop_slice: (a: number, b: number) => void;
+    readonly __wbindgen_start: () => void;
 }
 
 export type SyncInitInput = BufferSource | WebAssembly.Module;
@@ -74,3 +89,5 @@ export function initSync(module: { module: SyncInitInput } | SyncInitInput): Ini
 * @returns {Promise<InitOutput>}
 */
 export default function __wbg_init (module_or_path?: { module_or_path: InitInput | Promise<InitInput> } | InitInput | Promise<InitInput>): Promise<InitOutput>;
+
+export { AjisaiInterpreter };
