acd7f081312eeab7952c95f7a36145acee6ac737
masamoto yamashiro <masamoto@protonmail.com>
2025-08-07 22:53:56 +0900

    Update control.rs

--- rust/src/interpreter/control.rs
+++ rust/src/interpreter/control.rs
@@ -1,34 +1,11 @@
 use std::collections::HashSet;
-use crate::interpreter::{Interpreter, WordDefinition, error::{AjisaiError, Result}};
-use crate::types::{ValueType, Token};
+use crate::interpreter::{Interpreter, error::{AjisaiError, Result}};
+use crate::types::ValueType;
 
 pub fn op_def(interp: &mut Interpreter) -> Result<()> {
     // DEFは行末での特殊な構文として処理されるため、
     // 通常の実行フローでここに到達した場合はエラー
     Err(AjisaiError::from("DEF must be used at the end of a line with a string name: <words> \"NAME\" DEF"))
-}
-
-pub fn op_if(interp: &mut Interpreter) -> Result<()> {
-    if interp.stack.len() < 3 {
-        return Err(AjisaiError::StackUnderflow);
-    }
-    
-    let else_branch = interp.stack.pop().unwrap();
-    let then_branch = interp.stack.pop().unwrap();
-    let condition = interp.stack.pop().unwrap();
-
-    let (then_tokens, else_tokens) = match (&then_branch.val_type, &else_branch.val_type) {
-        (ValueType::Quotation(t), ValueType::Quotation(e)) => (t, e),
-        _ => return Err(AjisaiError::type_error("two quotations", "other types")),
-    };
-
-    let tokens_to_execute = match condition.val_type {
-        ValueType::Boolean(true) => then_tokens,
-        ValueType::Boolean(false) | ValueType::Nil => else_tokens,
-        _ => return Err(AjisaiError::type_error("boolean or nil", "other type")),
-    };
-    
-    interp.execute_tokens_with_context(tokens_to_execute)
 }
 
 pub fn op_del(interp: &mut Interpreter) -> Result<()> {
@@ -62,17 +39,5 @@ pub fn op_del(interp: &mut Interpreter) -> Result<()> {
             Ok(())
         },
         _ => Err(AjisaiError::type_error("string", "other type")),
-    }
-}
-
-pub fn op_call(interp: &mut Interpreter) -> Result<()> {
-    let val = interp.stack.pop()
-        .ok_or(AjisaiError::StackUnderflow)?;
-    
-    match val.val_type {
-        ValueType::Quotation(tokens) => {
-            interp.execute_tokens_with_context(&tokens)
-        },
-        _ => Err(AjisaiError::type_error("quotation", "other type")),
     }
 }
