3b77423aa8a2ef3de8dc6a8eab581b663e353eba
masamoto yamashiro <masamoto@protonmail.com>
2025-08-20 10:19:17 +0900

    Update leap.rs

--- rust/src/interpreter/leap.rs
+++ rust/src/interpreter/leap.rs
@@ -1,4 +1,4 @@
-// rust/src/interpreter/leap.rs
+// rust/src/interpreter/leap.rs (デバッグ版)
 
 use crate::interpreter::{Interpreter, error::{AjisaiError, Result}};
 use crate::types::{ValueType};
@@ -19,12 +19,16 @@ pub fn op_leap(interp: &mut Interpreter) -> Result<()> {
     let should_leap = match condition_val.val_type {
         ValueType::Boolean(b) => b,
         ValueType::Nil => false,
-        _ => true, // その他の値は真として扱う
+        _ => true,
     };
     
     if should_leap {
         // 現在実行中のワード名を取得
         let current_word = interp.call_stack.last().cloned();
+        
+        // デバッグ出力
+        interp.append_output(&format!("DEBUG: LEAP from {:?} to {}\n", current_word, word_name));
+        
         // 同一ワード内制限付きでワードを実行
         interp.execute_word_leap(&word_name, current_word.as_deref())?;
     }
