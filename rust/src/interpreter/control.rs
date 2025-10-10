00af03acf0793b3a16e8026c8bea172af1a63956
masamoto yamashiro <masamoto@protonmail.com>
2025-10-10 10:30:21 +0900

    Update control.rs

--- rust/src/interpreter/control.rs
+++ rust/src/interpreter/control.rs
@@ -1,7 +1,7 @@
 // rust/src/interpreter/control.rs
 
 use crate::interpreter::{Interpreter, error::{AjisaiError, Result}};
-use crate::types::{Token, ExecutionLine, ValueType, WordDefinition, Value};
+use crate::types::{Token, ExecutionLine, ValueType, WordDefinition};
 use std::collections::HashSet;
 use num_traits::{ToPrimitive, One};
 
