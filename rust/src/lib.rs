1cdaade72ae737e1c49ab7729e1f4df9c2e4d22b
masamoto yamashiro <masamoto@protonmail.com>
2025-10-11 18:47:16 +0900

    Update lib.rs

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -8,5 +8,7 @@ mod interpreter;
 mod builtins;
 mod wasm_api;
 
-#[wasm_bindgen]
+// `pub use` に `#[wasm_bindgen]` は適用できないため削除。
+// `AjisaiInterpreter` 構造体自体が `wasm_api.rs` の中で `#[wasm_bindgen]` されているため、
+// この `use` を介して正しくエクスポートされます。
 pub use wasm_api::AjisaiInterpreter;
